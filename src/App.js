6d3c80651aa2d67179e7e24b00938ef7cfb6c613
Paul Cedrick Artigo <paul.artigo@ugroop.com.au>
2017-05-25 18:04:39 +0800

    Added router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+import Header from './containers/Header/';
+import Footer from './containers/Footer/';
+import Homepage from './containers/Homepage/';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Header />
+          <Route exact path="/" component={Homepage} />
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code>
-        </p>
-      </div>
+      </Router>
     );
   }
 }
